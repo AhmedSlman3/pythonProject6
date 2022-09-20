30a88e38166431d9b1ed78d38bbfcfcedd2d235d
sam <donleth2088@gmail.com>
2022-09-20 21:57:57 +0300

    home work Q1

---
+++ Q1_hw.py
@@ -0,0 +1,17 @@
+numbers = [12, 75, 150, 180, 145, 525, 50]
+for num_ in numbers :
+    if num_ > 500 :
+        break
+    elif num_ <=150 and num_ % 5 == 0 :
+        print (num_)
+
+
+
+
+
+
+
+
+
+
+
